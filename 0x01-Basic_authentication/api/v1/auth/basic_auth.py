1e1a87ff244d27afa79c5f3c76a866586b5049b2
BrahimElhajji <elhajji080898@gmail.com>
2024-09-03 19:09:31 -0700

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Module for Basic Authentication """
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -22,13 +23,29 @@ class BasicAuth(Auth):
         Returns:
             str: The Base64 part of the header, or None if invalid.
         """
-        if authorization_header is None:
+        if authorization_header is None
+        or not isinstance(authorization_header, str):
             return None
-
-        if not isinstance(authorization_header, str):
-            return None
-
         if not authorization_header.startswith("Basic "):
             return None
-
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes the Base64 string to a UTF-8 string.
+
+        Args:
+            base64_authorization_header (str): The Base64 string to decode.
+
+        Returns:
+            str: The decoded string, or None if invalid.
+        """
+        if base64_authorization_header is None
+        or not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
