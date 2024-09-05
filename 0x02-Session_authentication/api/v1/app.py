a4aa9941d8f21cf4bffc6bd185c48b98f544c165
BrahimElhajji <elhajji080898@gmail.com>
2024-09-05 14:01:27 -0700

    Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,6 +6,7 @@ from os import getenv
 from flask_cors import CORS, cross_origin
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from models import storage
 
 
 app = Flask(__name__)
@@ -17,43 +18,34 @@ auth_type = getenv('AUTH_TYPE')
 
 if auth_type == "basic_auth":
     auth = BasicAuth()
+elif auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
 def before_request():
-    """ Before request handler to run before each request """
+    """Before each request handler"""
     if auth is None:
         return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, excluded_paths):
+    request.current_user = auth.current_user(request)
+    if not auth.require_auth(request.path, ['/api/v1/status/',
+                                            '/api/v1/unauthorized/',
+                                            '/api/v1/forbidden/']):
         return
     if auth.authorization_header(request) is None:
-        abort(401)
-    request.current_user = auth.current_user(request)
+        return jsonify({"error": "Unauthorized"}), 401
     if request.current_user is None:
-        abort(403)
+        return jsonify({"error": "Forbidden"}), 403
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler """
-    return jsonify({"error": "Not found"}), 404
-
-
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized handler """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden handler """
-    return jsonify({"error": "Forbidden"}), 403
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close storage on app context teardown"""
+    storage.close()
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=int(port))
