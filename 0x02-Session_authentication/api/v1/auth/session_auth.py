87357b95937de08b63329cc444f8c65f15c1b9c8
BrahimElhajji <elhajji080898@gmail.com>
2024-09-05 14:17:30 -0700

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,28 +1,57 @@
 #!/usr/bin/env python3
+
+"""
+session authentication
+"""
+
 from api.v1.auth.auth import Auth
-import uuid
+from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
-    """Session Authentication class that inherits from Auth."""
-
+    """
+    session authentication implementation
+    """
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
         """
-        Creates a Session ID for a given user_id.
-
-        Args:
-            user_id (str): The user ID for which to create a session.
-
-        Returns:
-            str: The generated Session ID, or None if input is invalid.
+        creates a session Id for a user
         """
-        if user_id is None or not isinstance(user_id, str):
+        if not user_id or not isinstance(user_id, str):
             return None
+        usr_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[usr_id] = user_id
+        return usr_id
 
-        session_id = str(uuid.uuid4())
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        returns user_id based on session_id
+        """
+        if not session_id or not isinstance(session_id, str):
+            return None
+        return SessionAuth.user_id_by_session_id.get(session_id)
 
-        self.user_id_by_session_id[session_id] = user_id
+    def current_user(self, request=None):
+        """
+        returns user based on cookie value
+        """
+        sesh_cookie = self.session_cookie(request)
+        usr_id = self.user_id_for_session_id(sesh_cookie)
+        return User.get(usr_id)
 
-        return session_id
+    def destroy_session(self, request=None):
+        """
+        logout user
+        """
+        if not request:
+            return False
+        sesh_cookie = self.session_cookie(request)
+        if not sesh_cookie:
+            return False
+        usr_id = self.user_id_for_session_id(sesh_cookie)
+        if not usr_id:
+            return False
+        SessionAuth.user_id_by_session_id.pop(sesh_cookie)
+        return True
