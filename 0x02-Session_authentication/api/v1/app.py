46a1fd5945d7746c425ba5b6537851e6dc014677
BrahimElhajji <elhajji080898@gmail.com>
2024-09-05 16:52:45 -0700

    et moi

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,68 +1,47 @@
 #!/usr/bin/env python3
-"""
-Route module for the API
-"""
-from os import getenv
+"""API application module"""
+from flask import Flask, jsonify, request
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
+from models import storage
+from os import getenv
 from flask_cors import CORS
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+app.register_blueprint(app_views)
+
 
 auth = None
-
-auth_type = getenv("AUTH_TYPE")
-if auth_type == "auth":
-    auth = Auth()
-elif auth_type == "basic_auth":
+if getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
 
 
-@app.errorhandler(401)
-def unauthorized_error(error) -> str:
-    """ Unauthorized error handler """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden_error(error) -> str:
-    """ Forbidden error handler """
-    return jsonify({"error": "Forbidden"}), 403
-
-
 @app.before_request
-def before_request_handler():
-    """Handler for filtering requests before they reach the view"""
+def before_request():
+    """Before each request handler"""
     if auth is None:
         return
-
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
-
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-
-    if auth.authorization_header(request) is None
-    and auth.session_cookie(request) is None:
-        abort(401)
-
     request.current_user = auth.current_user(request)
+    if not auth.require_auth(request.path, ['/api/v1/status/',
+                                            '/api/v1/unauthorized/',
+                                            '/api/v1/forbidden/']):
+        return
+    if auth.authorization_header(request) is None:
+        return jsonify({"error": "Unauthorized"}), 401
     if request.current_user is None:
-        abort(403)
+        return jsonify({"error": "Forbidden"}), 403
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler """
-    return jsonify({"error": "Not found"}), 404
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close storage on app context teardown"""
+    storage.close()
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=int(port))
