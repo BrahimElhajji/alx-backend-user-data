7ec91e78f7c71e8aacbe1fa44e6f1154dc9dee31
BrahimElhajji <elhajji080898@gmail.com>
2024-09-06 03:05:14 +0000

    fixing task 7

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,7 +4,7 @@ view to handle all routes for the Session authentication
 """
 
 from api.v1.views import app_views
-from flask import json, request, jsonify, abort
+from flask import request, jsonify, abort
 from models.user import User
 from os import getenv
 from api.v1.app import auth
@@ -13,33 +13,40 @@ from api.v1.app import auth
 @app_views.route("/auth_session/login", methods=['POST'], strict_slashes=False)
 def sesh_login():
     """
-    handle login
+    Handle user login with session authentication.
     """
     usr_email = request.form.get("email")
     usr_pass = request.form.get("password")
+
     if not usr_email:
         return jsonify({"error": "email missing"}), 400
+
     if not usr_pass:
         return jsonify({"error": "password missing"}), 400
 
     search_res = User.search({"email": usr_email})
     if not search_res:
         return jsonify({"error": "no user found for this email"}), 404
+
     my_usr = search_res[0]
+
     if not my_usr.is_valid_password(usr_pass):
         return jsonify({"error": "wrong password"}), 401
-    from api.v1.app import auth
-    cookie = auth.create_session(my_usr.id)
+
+    session_id = auth.create_session(my_usr.id)
     response = jsonify(my_usr.to_json())
-    response.set_cookie(getenv("SESSION_NAME", cookie))
+
+    session_name = getenv("SESSION_NAME")
+    response.set_cookie(session_name, session_id)
+
+    return response
 
 
 @app_views.route("/auth_session/logout",
-                 methods=['DELETE'],
-                 strict_slashes=False)
+                 methods=['DELETE'], strict_slashes=False)
 def sesh_logout():
     """
-    handle logout
+    Handle user logout by destroying the session.
     """
     if not auth.destroy_session(request):
         abort(404)
