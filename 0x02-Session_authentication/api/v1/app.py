e987f705181578380af27de9384dbd51b79cdb13
BrahimElhajji <elhajji080898@gmail.com>
2024-09-05 17:29:31 -0700

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,50 +9,66 @@ from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 app.register_blueprint(app_views)
-
-# Initialize auth based on AUTH_TYPE environment variable
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-auth_type = getenv('AUTH_TYPE')
+auth_type = getenv("AUTH_TYPE")
 
 if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 elif auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
+else:
+    auth = Auth()
 
 
 @app.before_request
-def require_auth():
+def filter_auth_require() -> None:
     """
-    Method to enforce authentication.
+    filters requests that need authorization
     """
-    excluded_paths = ['/api/v1/status', '/api/v1/auth_session/login/']
-
-    if request.path not in excluded_paths:
-        if not (auth.authorization_header(request)
-                or auth.session_cookie(request)):
-            abort(401)
+    require_auth = auth.require_auth(request.path,
+                                     ['/api/v1/status/',
+                                      '/api/v1/unauthorized/',
+                                      '/api/v1/forbidden/',
+                                      '/api/v1/auth_session/login/'])
+    if not require_auth:
+        return None
+    if not auth.authorization_header(
+            request) and not auth.session_cookie(request):
+        abort(401)
+    usr = auth.current_user(request)
+    if not usr:
+        abort(403)
+    request.current_user = usr
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler """
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler """
+    """
+        Not authorized error
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden handler """
+    """
+        forbidden error
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
-    app.run(host=getenv('API_HOST', '0.0.0.0'),
-            port=int(getenv('API_PORT', 5000)))
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
