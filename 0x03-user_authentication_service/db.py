3a77415df1ee113308c474bdeaedaa43448df31a
BrahimElhajji <elhajji080898@gmail.com>
2024-09-12 02:15:29 +0000

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,9 +6,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+
 from user import Base, User
 
 
@@ -43,14 +43,10 @@ class DB:
         Returns:
             User: The newly created user object.
         """
-        try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            self._session.add(new_user)
-            self._session.commit()
-            return new_user
-        except SQLAlchemyError as e:
-            self._session.rollback()
-            raise e
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
 
     def find_user_by(self, **kwargs) -> User:
         """Find a user by arbitrary keyword arguments.
@@ -72,3 +68,23 @@ class DB:
             raise NoResultFound("No user found with the given parameters.")
         except InvalidRequestError:
             raise InvalidRequestError("Invalid query parameters provided.")
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user's attributes.
+
+        Args:
+            user_id (int): The ID of the user to update.
+            **kwargs: The attributes to update with their new values.
+
+        Raises:
+            ValueError: If a provided attribute does not exist
+            on the User model.
+        """
+        user = self.find_user_by(id=user_id)
+
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError(f"{key} is not a valid attribute of User")
+            setattr(user, key, value)
+
+        self._session.commit()
