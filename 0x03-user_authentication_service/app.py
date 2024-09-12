0600790a831b71a4c1264dae910699d0fd6099bc
BrahimElhajji <elhajji080898@gmail.com>
2024-09-12 21:18:40 +0000

    Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,19 +63,6 @@ def logout():
     return redirect('/')
 
 
-def update_password():
-    """Handles password update using a reset token."""
-    email = request.form.get("email")
-    reset_token = request.form.get("reset_token")
-    new_password = request.form.get("new_password")
-
-    try:
-        AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"})
-    except ValueError:
-        abort(403)
-
-
 @app.route('/profile', methods=['GET'])
 def profile():
     """Handles requests to get the user's profile information."""
@@ -101,6 +88,20 @@ def reset_password():
     try:
         reset_token = AUTH.get_reset_password_token(email)
         return jsonify({"email": email, "reset_token": reset_token})
+    except ValueError:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'])
+def update_password():
+    """Handles password update using a reset token."""
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
     except ValueError:
         abort(403)
 
